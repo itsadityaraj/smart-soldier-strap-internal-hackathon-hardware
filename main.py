95c922ce48c86d1c43d8f2dfc7549db6d8f20ffc
Aditya Rajput <rajaditya3998@gmail.com>
2020-02-01 13:26:54 +0530

    initial setup of firebase

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyrebase
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com"
+}
