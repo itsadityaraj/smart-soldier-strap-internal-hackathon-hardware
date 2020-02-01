1d215a095a6967094c5bf958ad80e464accf7cff
Aditya Rajput <rajaditya3998@gmail.com>
2020-02-01 14:58:19 +0530

    Added some methods in main.py

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from soldier import soldier
 firebase = FC.firebaseMain
 
 db = firebase.database()
-print(db)
-
-
-data = {"name": "Vishesh"
-        }
-
-db.child("soldier_data").child("Soldier Id").set(data)
-
 users = db.child("soldier_data").get()
-for user in users.each():
-    print(user.key()) # Morty
-    print(user.val()) # {name": "Mortimer 'Morty' Smith"}
 
 
-# method to enter soldier entry in database 
 
+# method to extract all soldier entry in database 
+def all_soldier_id():
+    for user in users.each():
+        print(user.key())
+
+
+
+# method to extract soldier data using his name
+def search_soldier(args):
+    user = db.child("soldier_data").child(args).get()
+    if user.val() is None:
+        print("No such Solider Found")
+    else:
+        print(user.val())
+
+
+#data = {"name": "Vishesh"}
+#db.child("soldier_data").child("Soldier Id").set(data)
+
+all_soldier_id()
+search_soldier("Rameshh")
