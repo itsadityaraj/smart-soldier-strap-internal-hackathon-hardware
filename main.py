eeaa528700c4a64b68ce7500726216d58e39b6f8
Aditya Rajput <rajaditya3998@gmail.com>
2020-02-01 14:37:13 +0530

    created separate soldier class for organizing information

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import firebaseConfig as FC
+from soldier import soldier
 
 firebase = FC.firebaseMain
 
@@ -18,3 +19,7 @@ users = db.child("soldier_data").get()
 for user in users.each():
     print(user.key()) # Morty
     print(user.val()) # {name": "Mortimer 'Morty' Smith"}
+
+
+# method to enter soldier entry in database 
+
---
+++ soldier.py
@@ -0,0 +1,10 @@
+class soldier:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def soldier_name(self):
+        return self.name
+
+    def solider_id(self):
+        return self.id
